6779af32740a2a831fc23336e45c4b10a378c742
Son Nguyen <Son.mofa415@gmail.com>
2021-11-18 12:20:50 -0500

    Splitting

--- Driver.java
+++ Driver.java
@@ -1,16 +1,32 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.*;
 
 public class Driver{
     
 
+    public static String[] removeQuotation(String[] strArray) {
+        for(int i = 0; i < strArray.length; i++) {
+            String newStr = "";
+            for(int j = 0; j < strArray[i].length(); j++){
+                if(strArray[i].charAt(j) != '"') {
+                    newStr += strArray[i].charAt(j);
+                }
+            }
+            strArray[i] = newStr;
+        }
+        return strArray;
+    }
     public static void main(String[] args) {
 
-        // Code section courtesy of Baeldung and W3School
+
+
+        // Scanner and try except courtesy of Baeldung and W3School
         try (Scanner scanner = new Scanner(new File("testdata.csv"));) {
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                String myLine = scanner.nextLine();
+                String[] strArray = myLine.split("\",\"");
+                removeQuotation(strArray);
+                System.out.println(Arrays.toString(strArray));
             }
         } catch(FileNotFoundException e) {
             System.out.println("File not found");
