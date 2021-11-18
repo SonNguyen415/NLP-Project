3b5fb223ca5ea0eba78d9cef16a50098f8e9eb89
Son Nguyen <Son.mofa415@gmail.com>
2021-11-18 11:49:08 -0500

    Exception Handling

--- Driver.java
+++ Driver.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 import java.io.File;
-
+import java.io.FileNotFoundException;
 
 public class Driver{
     
 
     public static void main(String[] args) {
 
-        // Code section courtesy of Baeldung 
+        // Code section courtesy of Baeldung and W3School
         try (Scanner scanner = new Scanner(new File("testdata.csv"));) {
             while (scanner.hasNextLine()) {
                 System.out.println(scanner.nextLine());
             }
-        } catch(Exception fileNotFoundException) {
+        } catch(FileNotFoundException e) {
             System.out.println("File not found");
         }
     }
