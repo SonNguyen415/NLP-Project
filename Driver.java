e5a95fcf771aad9efcb7f54f609b5d4bb3e7af80
Son Nguyen <Son.mofa415@gmail.com>
2021-11-18 11:43:31 -0500

    FIle read

--- Driver.java
+++ Driver.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+
+
 public class Driver{
+    
 
     public static void main(String[] args) {
 
+        // Code section courtesy of Baeldung 
+        try (Scanner scanner = new Scanner(new File("testdata.csv"));) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch(Exception fileNotFoundException) {
+            System.out.println("File not found");
+        }
     }
 
 }
