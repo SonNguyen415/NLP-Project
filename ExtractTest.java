d174a58028f8dce8d0404ce81517befb0d856c6c
Son Nguyen <Son.mofa415@gmail.com>
2021-11-30 12:06:27 -0500

    yes

--- ExtractTest.java
+++ ExtractTest.java
@@ -1,40 +1,40 @@
-// public class ExtractTest {
+public class ExtractTest {
 
-//   public static void testChecker() {
+  public static void testChecker() {
     
-//   }
+  }
   
 
-//   public static void main(String[] args) {
-//     boolean test2 = true;
-//     boolean completed = true;
+  public static void main(String[] args) {
+    boolean test2 = true;
+    boolean completed = true;
     
-//     if (test2 == true){
-//         completed = true;
-//         try{completed = testChecker("main1", "test2", main1("Mon", "May", "11 03:17:40 UTC 2009"), "Mon May 11 03:17:40 UTC 2009",  "May","Mon","2009") && completed;
-//     }catch (Exception e){System.out.println("Oops! Check your main1, test1, on inputs Mon May 11 03:17:40 UTC 2009 -- it raised an exception.");e.printStackTrace();}
+    if (test2 == true){
+        completed = true;
+        try{completed = testChecker("main1", "test2", main1("Mon", "May", "11 03:17:40 UTC 2009"), "Mon May 11 03:17:40 UTC 2009",  "May","Mon","2009") && completed;
+    }catch (Exception e){System.out.println("Oops! Check your main1, test1, on inputs Mon May 11 03:17:40 UTC 2009 -- it raised an exception.");e.printStackTrace();}
 
-//     }
-//   }
+    }
+  }
   
 
 
-// // possible example
-//     // Problem 1 test cases
-//     /*if (test1 == true){
-//         completed = true;
-//         try{completed = testChecker("main1", "test1", main1("bird", "i"), "bird and i",  1) && completed;
-//           }catch (Exception e){System.out.println("Oops! Check your main1, test1, on inputs bird and i -- it raised an exception.");e.printStackTrace();}
-//         try{completed = testChecker("main1", "test2", main1("bird", "x"), "bird and x",  0) && completed;
-//           }catch (Exception e){System.out.println("Oops! Check your main1, test2, on inputs bird and x -- it raised an exception.");e.printStackTrace();}
-//         try{completed = testChecker("main1", "test3", main1("ibiridi", "i"), "ibiridi and i",  4) && completed;
-//           }catch (Exception e){System.out.println("Oops! Check your main1, test3, on inputs ibiridi and i -- it raised an exception.");e.printStackTrace();}
-//         try{completed = testChecker("main1", "test4", main1("iaebiaeriaediae", "iae"), "iaebiaeriaediae and iae",  4) && completed;
-//           }catch (Exception e){System.out.println("Oops! Check your main1, test4, on inputs iaebiaeriaediae and iae -- it raised an exception.");e.printStackTrace();}
-//         if (completed == true)
-//           System.out.println("Problem 1 finished, great work!");
-//         else
-//           System.out.println("Check the errors above for Problem 1, and try again!");
-//       }else System.out.println("skipping test1");
-//       */
-// }
+// possible example
+    // Problem 1 test cases
+    /*if (test1 == true){
+        completed = true;
+        try{completed = testChecker("main1", "test1", main1("bird", "i"), "bird and i",  1) && completed;
+          }catch (Exception e){System.out.println("Oops! Check your main1, test1, on inputs bird and i -- it raised an exception.");e.printStackTrace();}
+        try{completed = testChecker("main1", "test2", main1("bird", "x"), "bird and x",  0) && completed;
+          }catch (Exception e){System.out.println("Oops! Check your main1, test2, on inputs bird and x -- it raised an exception.");e.printStackTrace();}
+        try{completed = testChecker("main1", "test3", main1("ibiridi", "i"), "ibiridi and i",  4) && completed;
+          }catch (Exception e){System.out.println("Oops! Check your main1, test3, on inputs ibiridi and i -- it raised an exception.");e.printStackTrace();}
+        try{completed = testChecker("main1", "test4", main1("iaebiaeriaediae", "iae"), "iaebiaeriaediae and iae",  4) && completed;
+          }catch (Exception e){System.out.println("Oops! Check your main1, test4, on inputs iaebiaeriaediae and iae -- it raised an exception.");e.printStackTrace();}
+        if (completed == true)
+          System.out.println("Problem 1 finished, great work!");
+        else
+          System.out.println("Check the errors above for Problem 1, and try again!");
+      }else System.out.println("skipping test1");
+      */
+}
