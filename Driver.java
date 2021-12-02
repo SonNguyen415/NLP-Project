7b3a77de744a70150b1622b3734182e7790ccf98
Renaud2002 <52769325+Renaud2002@users.noreply.github.com>
2021-12-02 13:33:37 -0500

    hashmap done

--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 import java.util.*;
+
+import javax.print.attribute.HashPrintJobAttributeSet;
+
 import java.io.*;
 
 
@@ -22,6 +25,41 @@ public class Driver{
         } catch(FileNotFoundException e) {
             System.out.println("File not found");
         }
+
+        HashMap<String ,Integer> wordctr = new HashMap<String ,Integer>();
+
+        int ctr =1;
+
+        for(int i = 0; i< sentenceList.size(); i++){
+           Sentence sen = sentenceList.get(i); // to retrive a sentence from the list
+           ArrayList<String> words = sen.splitSentence();
+           for(int j = 0;j < words.size(); j++){
+               String word = words.get(j);
+               if(wordctr.containsKey(word)){
+                   wordctr.replace(word, ctr, ctr++);
+               }else{
+                   wordctr.put(word, ctr);
+               }
+           }
+           
+        }
+
+        Map.Entry<String, Integer> maxEntry = null;
+for (Map.Entry<String, Integer> entry : wordctr.entrySet())
+    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+        maxEntry = entry;
+int maxValueLen = maxEntry.getValue().toString().length();
+ArrayList <String> results = new ArrayList<String>();
+for (Map.Entry set : wordctr.entrySet()){
+    String value = set.getValue().toString();
+    while(value.length() < maxValueLen)
+        value = " " + value;
+    results.add(value + " of " + set.getKey());
+}
+Collections.sort(results);
+Collections.reverse(results);
+for (int i = 0; i < 100; i++)
+    System.out.println(results.get(i));
     }
 
 }
