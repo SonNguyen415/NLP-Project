924fca3524b29936315fde660b7ed263d958e3c7
Son Nguyen <Son.mofa415@gmail.com>
2021-12-02 18:35:44 -0500

    fix hashmap problem

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-import javax.print.attribute.HashPrintJobAttributeSet;
+// import javax.print.attribute.HashPrintJobAttributeSet;
 
 import java.io.*;
 
@@ -21,7 +21,6 @@ public class Driver{
                 String myLine = scanner.nextLine();
                 Sentence newSentence = Sentence.convertLine(myLine);
                 sentenceList.add(newSentence);
-                System.out.println(newSentence);
             }
         } catch(FileNotFoundException e) {
             System.out.println("File not found");
@@ -32,35 +31,38 @@ public class Driver{
         int ctr =1;
 
         for(int i = 0; i< sentenceList.size(); i++){
-           Sentence sen = sentenceList.get(i); // to retrive a sentence from the list
+           Sentence sen = sentenceList.get(i); // to retrieve a sentence from the list
            ArrayList<String> words = sen.splitSentence();
-           for(int j = 0;j < words.size(); j++){
+           for(int j = 0; j < words.size(); j++){
                String word = words.get(j);
                if(wordctr.containsKey(word)){
                    wordctr.replace(word, ctr, ctr++);
                }else{
                    wordctr.put(word, ctr);
                }
-           }
-           
+           }   
         }
 
-        Map.Entry<String, Integer> maxEntry = null;
-for (Map.Entry<String, Integer> entry : wordctr.entrySet())
-    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
-        maxEntry = entry;
-int maxValueLen = maxEntry.getValue().toString().length();
-ArrayList <String> results = new ArrayList<String>();
-for (Map.Entry set : wordctr.entrySet()){
-    String value = set.getValue().toString();
-    while(value.length() < maxValueLen)
-        value = " " + value;
-    results.add(value + " of " + set.getKey());
-}
-Collections.sort(results);
-Collections.reverse(results);
-for (int i = 0; i < 100; i++)
-    System.out.println(results.get(i));
-    }
 
+
+
+        Map.Entry<String, Integer> maxEntry = null;
+        for (Map.Entry<String, Integer> entry : wordctr.entrySet())
+            if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0)
+                maxEntry = entry;
+        int maxValueLen = maxEntry.getValue().toString().length();
+        ArrayList <String> results = new ArrayList<String>();
+        for (Map.Entry set : wordctr.entrySet()){
+            String value = set.getValue().toString();
+            while(value.length() < maxValueLen)
+                value = " " + value;
+            results.add(value + " of " + set.getKey());
+        }
+        Collections.sort(results);
+        Collections.reverse(results);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(results.get(i));
+        }
+        System.out.println("Finished");
+    }
 }
